96c97d923259834f954d73ce4c93efb9dd2ccbce
Elliot Miller <elliot@bitoffdev.com>
2019-10-25 22:48:44 -0400

    Use portable asm/socket.h header rather than architecture-specific sys header

--- src/server.c
+++ src/server.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <asm/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <netdb.h>
